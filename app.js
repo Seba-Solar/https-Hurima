8f0a08c0aead768106f1e41bcb9a6c6e6398d896
seba-solar <seba.solar@duocuc.cl>
2024-04-26 16:11:12 -0400

    Organizacion en carpetas

--- app.js
+++ app.js
@@ -5,23 +5,20 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/anuncio-crear.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'anuncio-crear.html'));
+  res.sendFile(path.join(__dirname, 'routes/anuncio-crear.html'));
 });
 
+
 router.get('/index', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'routes/index.html'));
 });
 
 app.get("/" , (req,res) => {
 	res.redirect('/login.html');
 });
 router.get('/login.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'login.html'));
+  res.sendFile(path.join(__dirname, 'routes/login.html'));
 });
-
-
-
-
 
 app.use('/', router);
 const port = process.env.PORT || 8082;
