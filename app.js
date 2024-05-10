4c16552b0349aa776a62d73a41029291bae15cd3
seba-solar <seba.solar@duocuc.cl>
2024-05-09 23:23:07 -0400

    Implementamos la funcionalidad del registro de anuncios

--- app.js
+++ app.js
@@ -1,41 +1,65 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const multer = require('multer');
-const mysql = require('mysql');
+  const express = require('express');
+  const app = express();
+  const path = require('path');
+  const mysql = require('mysql2');
+  const port = 8082;
 
-const router = express.Router();
+  //IMPORTANTE NO BORRAR
+  app.use(express.urlencoded({ extended: true })); // Middleware para parsear los datos del formulario
 
-// Configurar el tipo MIME para archivos JavaScript
-app.use('/js', express.static(__dirname + '/js', { 'Content-Type': 'text/javascript' }));
-app.use(express.static(__dirname + '/js', { 'Content-Type': 'text/javascript' }));
+  //Configurar base de datos
+
+  let conexion = mysql.createConnection({
+    host: "localhost",
+    database: "httpshurima",
+    user: "root",
+    password: ""
+  });
+
+  conexion.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
 
 
-router.get('/registro.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'registro.html'));
+  // ------------------------ ROUTING ------------------------ //
+  app.get('/registro.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'registro.html'));
+  });
+
+  app.get('/anuncio-crear.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'routes/anuncio-crear.html'));
+  });
+
+  app.post('/formulario', (req, res) => {
+    const nombre = req.body.nombre;
+    const descripcion = req.body.descripcion;
+
+  // Crea la consulta SQL
+    const sqlQuery = `INSERT INTO anuncios (nombre, descripcion) VALUES ('${nombre}', '${descripcion}')`;
+    conexion.query(sqlQuery, (error, results, fields) => {
+      if (error) {
+          return console.error('Error al ejecutar la consulta:', error.message);
+      }
+      console.log('Consulta ejecutada con éxito');
+      res.sendFile(path.join(__dirname, 'routes/index.html'));
+  });
 });
 
-router.get('/anuncio-crear.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'routes/anuncio-crear.html'));
+
+  app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, 'routes/index.html'));
+  });
+
+
+  app.get('/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'routes/login.html'));
+  });
+
+  app.get("/" , (req, res) => {
+    res.redirect('/index');
+  });
+
+  app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
 });
-
-
-router.get('/index', (req, res) => {
-  res.sendFile(path.join(__dirname, 'routes/index.html'));
-});
-
-
-router.get('/login.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'routes/login.html'));
-});
-
-app.get("/" , (req, res) => {
-	res.redirect('/index');
-});
-
-
-
-app.use('/', router);
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
